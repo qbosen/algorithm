4bc4c9f43902235fde6e476a30178c5eb2b3dda9
qiubaisen <abosen@qq.com>
2020-04-23 17:56:35 +0800

    硬币 面试题 08.11 背包问题

--- src/main/java/leetcode/editor/cn/CoinLcci.kt
+++ src/main/java/leetcode/editor/cn/CoinLcci.kt
@@ -1,34 +1,37 @@
 package leetcode.editor.cn
 
-import org.hamcrest.MatcherAssert.*
-import org.hamcrest.Matchers.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 
 // 硬币
 // 面试题 08.11
 // 5
 
 fun main() {
-    val result = Solution()::waysToChange
+    val result = CoinLcci.Solution()::waysToChange
     assertThat(result(5), equalTo(2))
     assertThat(result(10), equalTo(4))
 }
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    fun waysToChange(n: Int): Int {
-        //f(i,n)=f(i,n-ci)+f(i-1,n)
-        val mod = 1000000007
-        val coins = intArrayOf(1, 5, 10, 25)
-        val dp = IntArray(n + 1)
-        dp[0] = 1
+class CoinLcci {
 
-        for (coin in coins) {
-            for (i in coin..n){
-                dp[i] = (dp[i] + dp[i-coin]) % mod
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun waysToChange(n: Int): Int {
+            //f(i,n)=f(i,n-ci)+f(i-1,n)
+            val mod = 1000000007
+            val coins = intArrayOf(1, 5, 10, 25)
+            val dp = IntArray(n + 1)
+            dp[0] = 1
+
+            for (coin in coins) {
+                for (i in coin..n) {
+                    dp[i] = (dp[i] + dp[i - coin]) % mod
+                }
             }
+            return dp[n]
         }
-        return dp[n]
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
+}
