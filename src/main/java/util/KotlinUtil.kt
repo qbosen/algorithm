549714cf0ede2c06cd2dcb07e6a8f746c815bdac
qiubaisen <abosen@qq.com>
2020-04-24 10:24:40 +0800

    kotlin工具类

---
+++ src/main/java/util/KotlinUtil.kt
@@ -0,0 +1,14 @@
+package util
+
+import struct.ListNode
+
+/**
+ * @author qiubaisen
+ * @date 2020/4/24
+ */
+
+fun String.to1DIntArray(): IntArray = IntUtil.parse1DIntArray(this)
+fun String.to2DIntArray(): Array<IntArray> = IntUtil.parse2DIntArray(this)
+fun String.toListNode(): ListNode = IntUtil.parse1DInt2ListNode(this)
+fun String.to2DCharArray(): Array<CharArray> = CharUtil.parse2D4String(this)
+fun String.toStringList(): List<String> = StringUtil.parse1DList(this)
