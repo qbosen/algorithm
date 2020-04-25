4005af3d6e7baa39b34fc93333976e5f0b968978
abosen <abosen@qq.com>
2020-04-25 19:11:19 +0800

    工具类更新

--- src/main/java/util/KotlinUtil.kt
+++ src/main/java/util/KotlinUtil.kt
@@ -12,3 +12,5 @@ fun String.to2DIntArray(): Array<IntArray> = IntUtil.parse2DIntArray(this)
 fun String.toListNode(): ListNode = IntUtil.parse1DInt2ListNode(this)
 fun String.to2DCharArray(): Array<CharArray> = CharUtil.parse2D4String(this)
 fun String.toStringList(): List<String> = StringUtil.parse1DList(this)
+
+fun Int.factorial() = (1..this).reduce { a, b -> a * b }
