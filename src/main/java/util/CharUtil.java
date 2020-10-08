7750c80ed6923043dec5a2505b38002fd1bdaa3c
qbosen <abosen@qq.com>
2020-10-08 14:00:51 +0800

    344 反转字符串

--- src/main/java/util/CharUtil.java
+++ src/main/java/util/CharUtil.java
@@ -9,5 +9,7 @@ public class CharUtil {
     public static char[][] parse2D4String(String input) {
         return CommonUtil.parse(input, char[][].class);
     }
-
+    public static char[] parse1D4String(String input){
+        return CommonUtil.parse(input, char[].class);
+    }
 }
