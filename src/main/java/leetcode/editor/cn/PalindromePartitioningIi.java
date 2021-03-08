3ba26455c2a8080f200e56c5032ffc51076d0566
qiubaisen <abosen@qq.com>
2021-03-08 17:04:59 +0800

    132 分割回文串 II

--- src/main/java/leetcode/editor/cn/PalindromePartitioningIi.java
+++ src/main/java/leetcode/editor/cn/PalindromePartitioningIi.java
@@ -1,47 +1,34 @@
 package leetcode.editor.cn;
 
+import java.util.Arrays;
+
 //132 分割回文串 II
 /* "aab" */
 public class PalindromePartitioningIi {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        int n;
-        int f[][];
-        int[] dp;
-
         public int minCut(String s) {
-            n = s.length();
-            f = new int[n][n];
-            dp = new int[n];
+            int n = s.length();
+            int[] dp = new int[n];
+            Arrays.fill(dp, n - 1);
 
-            for (int i = 1; i < n; i++) {
-                if (isPalindrome(s, 0, i) == 1) {
-                    dp[i] = 0;
-                    continue;
-                }
-                int count = Integer.MAX_VALUE;
-                for (int j = 0; j < i; j++) {
-                    if (isPalindrome(s, j + 1, i) == 1) {
-                        count = Math.min(count, dp[j]);
-                    }
-                }
-                dp[i] = count + 1;
+            for (int i = 0; i < n; i++) {
+                palindromic(s, i, i, dp);
+                palindromic(s, i, i + 1, dp);
             }
             return dp[n - 1];
         }
 
-        public int isPalindrome(String s, int a, int b) {
-            if (f[a][b] != 0) return f[a][b];
-            if (a >= b) {
-                f[a][b] = 1;
-            } else if (s.charAt(a) == s.charAt(b) && isPalindrome(s, a + 1, b - 1) == 1) {
-                f[a][b] = 1;
-            } else {
-                f[a][b] = -1;
+        public void palindromic(String s, int i, int j, int[] dp) {
+            while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+                int cur = i == 0 ? 0 : dp[i - 1] + 1;
+                if (cur < dp[j]) {
+                    dp[j] = cur;
+                }
+                i--;
+                j++;
             }
-
-            return f[a][b];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
