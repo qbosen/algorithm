eebe00cfbca0e82b3c6b127056c788207050ebde
qiubaisen <abosen@qq.com>
2021-03-18 10:26:46 +0800

    206 反转链表

---
+++ src/main/java/leetcode/editor/cn/ReverseLinkedList.java
@@ -0,0 +1,41 @@
+package leetcode.editor.cn;
+
+//206 反转链表
+/* [1,2,3,4,5] */
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode current = head;
+
+            while (current != null) {
+                ListNode next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
