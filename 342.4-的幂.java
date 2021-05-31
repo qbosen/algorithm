e8c1310bb124fa98231bddffccb1f1fc4f6ff2b4
qiubaisen <abosen@qq.com>
2021-05-31 10:32:39 +0800

    [342] 4的幂

---
+++ 342.4-的幂.java
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=342 lang=java
+ *
+ * [342] 4的幂
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0xAAAAAAAA) == 0;
+    }
+}
+// @lc code=end
