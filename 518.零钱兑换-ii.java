6d91e009a32aae91334c0044e7a4203b39448b92
abosen <abosen@qq.com>
2021-06-10 22:14:31 +0800

    [518] 零钱兑换 II

---
+++ 518.零钱兑换-ii.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=java
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+class Solution {
+    public int change(int amount, int[] coins) {
+        // dp[i] : 总金额为i的组合数
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}
+// @lc code=end
+
