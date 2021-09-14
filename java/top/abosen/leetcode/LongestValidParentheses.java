be7c72439f141a1bcb4acdeaca2aa58872528bf8
qiubaisen <abosen@qq.com>
2021-09-14 13:57:18 +0800

    [32] 最长有效括号

--- java/top/abosen/leetcode/LongestValidParentheses.java
+++ java/top/abosen/leetcode/LongestValidParentheses.java
@@ -1,5 +1,8 @@
 package top.abosen.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //[32] 最长有效括号
 /* "(()" */
 public class LongestValidParentheses {
@@ -9,7 +12,7 @@ public class LongestValidParentheses {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int longestValidParentheses(String s) {
+        public int longestValidParentheses_dp(String s) {
             int[] dp = new int[s.length()];
             int ans = 0;
             for (int i = 1; i < s.length(); i++) {
@@ -20,6 +23,26 @@ public class LongestValidParentheses {
                         dp[i] = dp[i - 1] + ((i - 2 - dp[i - 1]) >= 0 ? dp[i - 2 - dp[i - 1]] : 0) + 2;
                     }
                     ans = Math.max(ans, dp[i]);
+                }
+            }
+            return ans;
+        }
+
+        public int longestValidParentheses(String s) {
+            Deque<Integer> deque = new ArrayDeque<>();
+            deque.push(-1);
+            int ans = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(') {
+                    deque.push(i);
+                } else {
+                    deque.pop();
+                    if (deque.isEmpty()) {
+                        deque.push(i);
+                    } else {
+                        ans = Math.max(ans, i - deque.peek());
+                    }
                 }
             }
             return ans;
