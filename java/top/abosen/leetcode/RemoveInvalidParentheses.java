a9844ecb59d83efc599b273b3b0c2faa0ad13902
qiubaisen <abosen@qq.com>
2021-10-27 16:28:43 +0800

    [301] 删除无效的括号

--- java/top/abosen/leetcode/RemoveInvalidParentheses.java
+++ java/top/abosen/leetcode/RemoveInvalidParentheses.java
@@ -1,68 +1,67 @@
 package top.abosen.leetcode;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 //[301] 删除无效的括号
 /* "()())()" */
 public class RemoveInvalidParentheses {
     public static void main(String[] args) {
         Solution solution = new RemoveInvalidParentheses().new Solution();
+        System.out.println(solution.removeInvalidParentheses("(a)())()"));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         char[] arr;
-        Set<String> result = new HashSet<>();
+        int resLen;
+        List<String> result = new ArrayList<>();
 
         public List<String> removeInvalidParentheses(String s) {
-            int leftRemove = 0, rightRemove = 0;
             arr = s.toCharArray();
+            int l = 0, r = 0;
             for (char c : arr) {
                 if (c == '(') {
-                    leftRemove++;
+                    l++;
                 } else if (c == ')') {
-                    if (leftRemove > 0) {
-                        leftRemove--;
+                    if (l > 0) {
+                        l--;
                     } else {
-                        rightRemove++;
+                        r++;
                     }
                 }
             }
-
+            resLen = arr.length - l - r;
             StringBuilder sb = new StringBuilder();
-            backtrack(0, 0, 0, leftRemove, rightRemove, sb);
-            return new ArrayList<>(result);
+            dfs(0, sb, 0);
+            return result;
         }
 
-        void backtrack(int index, int leftCount, int rightCount, int leftRemove, int rightRemove, StringBuilder path) {
-            if (index >= arr.length) {
-                if (leftRemove == 0 && rightRemove == 0) {
+        private void dfs(int index, StringBuilder path, int count) {
+            if (count < 0 || arr.length - index + path.length() < resLen) {
+                return;
+            }
+            if (index == arr.length) {
+                if (count == 0 && path.length() == resLen) {
                     result.add(path.toString());
                 }
                 return;
             }
             char c = arr[index];
-            // 1. 删除
-            if (c == '(' && leftRemove > 0) {
-                backtrack(index + 1, leftCount, rightCount, leftRemove - 1, rightRemove, path);
-            }
-            if (c == ')' && rightRemove > 0) {
-                backtrack(index + 1, leftCount, rightCount, leftRemove, rightRemove - 1, path);
-            }
-            // 2. 保留
-            path.append(c);
+
+            int length = path.length();
             if (c != '(' && c != ')') {
-                // 原样保留
-                backtrack(index + 1, leftCount, rightCount, leftRemove, rightRemove, path);
-            } else if (c == '(') {
-                backtrack(index + 1, leftCount + 1, rightCount, leftRemove, rightRemove, path);
-            } else if (c == ')' && leftCount > rightCount) {
-                backtrack(index + 1, leftCount, rightCount + 1, leftRemove, rightRemove, path);
+                dfs(index + 1, path.append(c), count);
+                path.deleteCharAt(path.length() - 1);
+                return;
             }
+            dfs(index + 1, path.append(c), c == '(' ? count + 1 : count - 1);
             path.deleteCharAt(path.length() - 1);
+
+            if ((length == 0 || path.charAt(length - 1) != c)) {
+                dfs(index + 1, path, count);
+            }
+
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
