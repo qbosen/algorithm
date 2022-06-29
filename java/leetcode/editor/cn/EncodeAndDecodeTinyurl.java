ad5da2274b4887036b565b6ce853cc3dc37c91bf
qiubaisen <abosen@qq.com>
2022-06-29 18:18:15 +0800

    [535] TinyURL 的加密与解密

---
+++ java/leetcode/editor/cn/EncodeAndDecodeTinyurl.java
@@ -0,0 +1,31 @@
+package leetcode.editor.cn;
+
+//[535] TinyURL 的加密与解密
+/* "https://leetcode.com/problems/design-tinyurl" */
+public class EncodeAndDecodeTinyurl {
+    public static void main(String[] args) {
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    public class Codec {
+
+
+        // valid chars `A-Za-z0-9-._~:/?#[]@!$&'()*+,;%=`
+        // Encodes a URL to a shortened URL.
+        public String encode(String longUrl) {
+            return longUrl;
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public String decode(String shortUrl) {
+            return shortUrl;
+        }
+    }
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
